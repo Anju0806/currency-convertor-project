57a3b0ceba822d13abc7e05c3e1dd47e281fe719
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-05-05 01:20:11 +0800

    chart section updated

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -39,10 +39,13 @@ $(function () {
 //call api and add chart
 let chartDisplayed = false;
 function displaychart(fromCountry,toCountry){
-   // Check if chart has already been displayed
+// Check if chart has already been displayed
    if (chartDisplayed) {
     return;
   }
+  // Set chartDisplayed flag to true
+  chartDisplayed = true;
+
   const start_date = "2022-09-01";
   const currentDate = new Date().toISOString().slice(0, 10);
   let myHeaders = new Headers();
@@ -52,20 +55,29 @@ function displaychart(fromCountry,toCountry){
     redirect: 'follow',
       headers: myHeaders
   };
-  fetch(`https://api.apilayer.com/exchangerates_data/timeseries?start_date=${start_date}&end_date=${currentDate}&base=${fromCountry}&symbols=${toCountry}`, requestOptions)
+  fetch(`https://api.apilayer.com/exchangerates_data/timeseries?start_date=${start_date}&end_date=${currentDate}&base=${fromCountry}&symbols=${toCountry}`,requestOptions)
   .then(response => response.json())
   .then(result => {
     const data = Object.entries(result.rates).map(([date, rates]) => ({ time: date, value: rates[toCountry] }));
-    const chart = LightweightCharts.createChart($("#chart-section")[0]);
-    const lineSeries = chart.addLineSeries();
-    const chartProperties ={
-      width:1000,
-      height:800,
-      timescale:{
-        timeVisible:true,
-        secondVisible:false,
+    const chart = LightweightCharts.createChart($("#chart-section")[0],{ width: 800, height: 400 });
+    chart.applyOptions({
+      layout: {
+        backgroundColor: '#FFFFFF',
+        textColor: '#333',
+        fontFamily: 'Arial',
+        fontSize: 12,
+        paddingBottom: 0,
+        paddingTop: 0,
+        paddingRight: 0,
+        paddingLeft: 0
+      },
+      watermark: {
+        visible: false
       }
-    }
+    });
+    $("#chart-section").css({ width: "800px", margin: "auto" });
+
+    const lineSeries = chart.addLineSeries();
     lineSeries.setData([]);
     //lineSeries.setData(ch);
     lineSeries.setData(data);
@@ -73,6 +85,35 @@ function displaychart(fromCountry,toCountry){
     // Set chartDisplayed flag to true
     chartDisplayed = true;
   })
+  
   .catch(error => console.log('error', error));
   }
+
+   //bulma loader show and hide functions
+   function showLoader() {
+    $("#progress").removeClass('is-hidden');
+}
+function hideLoader() {
+    $("#progress").addClass('is-hidden');
+}
+//show and disable functions for convert button while data is retrieved from API.
+function disableConvertButton() {
+    $("#convertbtn").prop("disabled", true);
+}
+function activateConvertButton() {
+    $("#convertbtn").prop("disabled", false);
+}
+
+
+// Add a media query to rotate the arrow icon
+const mediaQuery = window.matchMedia('(max-width: 768px)');
+function handleMediaQuery(event) {
+    if (event.matches) {
+        $("#arrow-icon").addClass('rotate');
+    } else {
+        $("#arrow-icon").removeClass('rotate');
+    }
+}
+mediaQuery.addListener(handleMediaQuery);
+handleMediaQuery(mediaQuery);
   });
